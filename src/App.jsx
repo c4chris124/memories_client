71db0dc899be66333577ae4f202793ac8288c278
Christian Castellanos <c4chris124@gmail.com>
2022-10-24 21:32:40 -0600

    imported components files, crated basic containers and typos for main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import {Container, AppBar, Typography, Grow, Grid} from '@mui/material'
+import memories from './images/memories.png'
+import Form from './components/Form/Form'
+import Posts from './components/Posts/Posts'
 const App = () => {
   return (
-    <div>App</div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color='inherit'>
+        <Typography variant='h2' align='center'>Memories</Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xd={12} sm={7}>
+              <Form/>
+            </Grid>
+            <Grid item xd={12} sm={4}>
+              <Posts/>
+            </Grid>
+          </Grid>
+        </Container>
+
+      </Grow>
+    </Container>
   )
 }
 
