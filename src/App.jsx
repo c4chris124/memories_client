577551ca48fcdefeeb6e8aab425e0f358c0b03a4
Christian Castellanos <c4chris124@gmail.com>
2022-10-25 20:39:34 -0600

    Formatting code, swap post --> to form position

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import {Container, AppBar, Typography, Grow, Grid} from '@mui/material'
-import memories from './images/memories.png'
-import Form from './components/Form/Form'
-import Posts from './components/Posts/Posts'
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
+import memories from "./images/memories.png";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import useStyles from "./styles";
 const App = () => {
+  const classes = useStyles();
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color='inherit'>
-        <Typography variant='h2' align='center'>Memories</Typography>
-        <img src={memories} alt="memories" height="60" />
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
             <Grid item xd={12} sm={7}>
-              <Form/>
+              <Posts />
             </Grid>
             <Grid item xd={12} sm={4}>
-              <Posts/>
+              <Form />
             </Grid>
           </Grid>
         </Container>
-
       </Grow>
     </Container>
-  )
-}
+  );
+};
 
-export default App
+export default App;
