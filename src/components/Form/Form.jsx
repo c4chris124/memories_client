e34bd100fcccdd17913e80a00c6ef9efbf9be373
Christian Castellanos <c4chris124@gmail.com>
2022-10-27 19:47:54 -0600

    Changed styles for, due to make styles no longer works with r18, created styled components, added state, and forms styles

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import useStyles from "./styles";
+import {useState} from 'react'
+import { useTheme  } from '@mui/material/styles';
+import {TextField, Button, Typography, Paper} from '@mui/material'
 
 const Form = () => {
-  // const classes = useStyles();
+  const theme=useTheme()
+  const [postData, setPostData] = useState({
+    creator: '',
+    title:'',
+    message: '',
+    tags: '',
+    selectedFile: ''
+  })
+
+  const handleSubmit=() => {
+
+  }
+
   return (
-    <div>Form</div>
+    <Paper sx={{'padding':theme.spacing(2)}}>
+     <form autoComplete='off' noValidate onSubmit={handleSubmit} sx={{display: 'flex',flexWrap: 'wrap', justifyContent: 'center'}}>
+      <Typography variant='h6'>Creating a Memory</Typography>
+      <TextField name='creator' variant='outlined' label='Creator' fullWidth value={postData.creator} onChange={(e) => setPostData({creator: e.target.value})} />
+     </form>
+    </Paper>
   )
 }
 
 export default Form
+// 1:13
